9507129aa52e205a2927958016b9aa86c6765965
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2017-11-30 15:46:05 +0100

    Added display for maximal inventory capacity.

--- src/main/webapp/js/module_inventory.js
+++ src/main/webapp/js/module_inventory.js
@@ -1,5 +1,23 @@
-var inventoryCtrl = function($scope, dhConfig, dhInventory) {
+var inventoryCtrl = function($scope, dhConfig, dhProfile, dhInventory, dhStats) {
     $scope.conf = dhConfig;
     $scope.inventory = {};
     dhInventory.get(function(res) { $scope.inventory = res.data; });
+    $scope.profile = {};
+    dhProfile.get(function(res) { $scope.profile = res.data; });
+    $scope.stats = {};
+    dhStats.get(function(res) { $scope.stats = res.data; });
+
+    $scope.totalWeight = function() {
+        var weight = 0;
+        if ($scope.inventory.gears) {
+            $scope.inventory.meleeWeapons.forEach(function(w) { weight += $scope.conf.meleeWeapons[w.name].weight });
+            $scope.inventory.missileWeapons.forEach(function(w) { weight += $scope.conf.missileWeapons[w.name].weight });
+            $scope.inventory.gears.forEach(function(g) {
+                if ($scope.conf.armour[g.name]) weight += $scope.conf.armour[g.name].weight;
+                else if ($scope.conf.gears[g.name]) weight += $scope.conf.gears[g.name].weight;
+                else weight += 0.5;
+            });
+        }
+        return weight;
+    };
 };
